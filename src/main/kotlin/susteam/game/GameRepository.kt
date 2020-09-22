c0f51ece9dd0b66073a31b0657320fd12303f631
Gogo <gogo81745.moe@gmail.com>
2020-09-22 22:55:00 +0800

    fix: GameRepository

--- src/main/kotlin/susteam/game/GameRepository.kt
+++ src/main/kotlin/susteam/game/GameRepository.kt
@@ -12,57 +12,55 @@ import java.time.Instant
 class GameRepository @Inject constructor(private val database: JDBCClient) {
 
     suspend fun createGame(
-            name: String,
-            price: Int,
-            publishDate: Instant,
-            author: String,
-            description: String?
+        name: String,
+        price: Int,
+        publishDate: Instant,
+        author: String,
+        description: String?
     ): Int {
         try {
             return database.updateWithParamsAwait(
-                    """INSERT INTO game (name, price, publish_date, author, description) VALUES (?, ?, ?, ?, ?);""",
-                    jsonArrayOf(name, price, publishDate, author, description)
+                """INSERT INTO game (name, price, publish_date, author, description) VALUES (?, ?, ?, ?, ?);""",
+                jsonArrayOf(name, price, publishDate, author, description)
             ).keys.getInteger(0)
         } catch (e: SQLIntegrityConstraintViolationException) {
-                throw e
-        }
-    }
-
-    suspend fun updateDescription(
-            gameId: Int,
-            description: String?
-    ) {
-        try {
-            database.updateWithParamsAwait(
-                    """UPDATE game SET description=? WHERE id=?""",
-                    jsonArrayOf(description, gameId)
-            )
-        } catch (e: SQLIntegrityConstraintViolationException) {
-            if (e.message?.contains("game.id") == true) {
-                throw ServiceException("Game '$gameId' is not exist", e)
+            if (e.message?.contains("game.name") == true) {
+                throw ServiceException("Cannot create game '$name'", e)
             } else {
                 throw e
             }
         }
     }
 
+    suspend fun updateDescription(
+        gameId: Int,
+        description: String?
+    ): Boolean {
+        return database.updateWithParamsAwait(
+            """UPDATE game SET description = ? WHERE game_id = ?;""",
+            jsonArrayOf(description, gameId)
+        ).updated == 1
+    }
+
     suspend fun createVersion(
-            gameId: Int,
-            versionName: String,
-            url: String
+        gameId: Int,
+        versionName: String,
+        url: String
     ) {
         try {
-            val game: Game? = getById(gameId)
-            if (game == null) {
-                throw ServiceException("Cannot create version '$versionName' for game '$gameId' because game '$gameId' do not exist")
-            }
             database.updateWithParamsAwait(
-                    """INSERT INTO game_version (game_id, name, url) VALUES (?, ?, ?);""",
-                    jsonArrayOf(game.id, versionName, url)
+                """INSERT INTO game_version (game_id, name, url) VALUES (?, ?, ?);""",
+                jsonArrayOf(gameId, versionName, url)
             )
         } catch (e: SQLIntegrityConstraintViolationException) {
-            if (e.message?.contains("game_version.name") == true || e.message?.contains("game_version.url") == true) {
-                throw ServiceException("Cannot create game version '$versionName', already exist", e)
+            val message = e.message ?: throw e
+
+            if (message.contains("FOREIGN KEY (`game_id`)")) {
+                throw ServiceException("Cannot create version '$versionName' for game '$gameId', game '$gameId' do not exist")
+            } else if (message.contains("game_version.PRIMARY")) {
+                throw ServiceException("Cannot create version '$versionName', version name already exist", e)
+            } else if (message.contains("game_version.url")) {
+                throw ServiceException("Cannot create version '$versionName', version url already exist", e)
             } else {
                 throw e
             }
@@ -71,29 +69,24 @@ class GameRepository @Inject constructor(private val database: JDBCClient) {
 
     suspend fun getById(id: Int): Game? {
         return database.querySingleWithParamsAwait(
-                """SELECT game_id, name, price, publish_date, author, description FROM game WHERE name = ?;""",
-                jsonArrayOf(id)
+            """SELECT game_id, name, price, publish_date, author, description FROM game WHERE game_id = ?;""",
+            jsonArrayOf(id)
         )?.let {
             Game(
-                    it.getInteger(0), it.getString(1), it.getInteger(2),
-                    it.getInstant(3), it.getString(4), it.getString(5)
+                it.getInteger(0), it.getString(1), it.getInteger(2),
+                it.getInstant(3), it.getString(4), it.getString(5)
             )
         }
     }
 
     suspend fun getVersion(gameId: Int, versionName: String): GameVersion? {
-        val game: Game? = getById(gameId)
-        if (game != null) {
-            return database.querySingleWithParamsAwait(
-                    """SELECT game_id, name, url FROM game_version WHERE name = ? and game_id = ?;""",
-                    jsonArrayOf(versionName, gameId)
-            )?.let {
-                GameVersion(
-                        it.getInteger(0), it.getString(1), it.getString(2)
-                )
-            }
-        } else {
-            throw ServiceException("Game not found")
+        return database.querySingleWithParamsAwait(
+            """SELECT game_id, name, url FROM game_version WHERE name = ? AND game_id = ?;""",
+            jsonArrayOf(versionName, gameId)
+        )?.let {
+            GameVersion(
+                it.getInteger(0), it.getString(1), it.getString(2)
+            )
         }
     }
 
