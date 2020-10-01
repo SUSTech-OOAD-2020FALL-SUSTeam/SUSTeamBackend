bc8893a0519ec9c1f96b5bb06e594bfc304f05c9
yinpe <11810305@mail.sustech.edu.cn>
2020-10-01 20:39:39 +0800

    Add data class 'GameProfile', 'GameDetail' and their functions.

--- src/main/kotlin/susteam/game/Game.kt
+++ src/main/kotlin/susteam/game/Game.kt
@@ -10,6 +10,7 @@ data class Game(
         val price: Int,
         val publishDate: Instant,
         val author: String,
+        val introduction: String?,
         val description: String?
 )
 
@@ -19,12 +20,31 @@ data class GameVersion(
         val url: String
 )
 
+data class GameProfile(
+        val id: Int,
+        val name: String,
+        val price: Int,
+        val publishDate: Instant,
+        val author: String,
+        val introduction: String?,
+        // description数据量比较大，GameProfile 中没有 description
+        val imageFullSize: String,
+        val imageCardSize: String
+)
+
+data class GameDetail(
+        val game: Game,
+        val images: List<String>
+        //TODO 还没写一个类覆盖string，格式暂时用String存
+)
+
 fun Game.toJson(): JsonObject = jsonObjectOf(
         "id" to id,
         "name" to name,
         "price" to price,
         "publishDate" to publishDate,
         "author" to author,
+        "introduction" to introduction,
         "description" to description
 )
 
@@ -34,6 +54,7 @@ fun JsonObject.toGame(): Game = Game(
         getInteger("price"),
         getInstant("publishDate"),
         getString("author"),
+        getString("introduction"),
         getString("description")
 )
 
@@ -47,4 +68,36 @@ fun JsonObject.toGameVersion(): GameVersion = GameVersion(
         getInteger("gameId"),
         getString("name"),
         getString("url")
+)
+
+fun GameProfile.toJson(): JsonObject = jsonObjectOf(
+        "id" to id,
+        "name" to name,
+        "price" to price,
+        "publishDate" to publishDate,
+        "author" to author,
+        "introduction" to introduction,
+        "imageFullSize" to imageFullSize,
+        "imageCardSize" to imageCardSize
+)
+
+fun JsonObject.toGameProfile(): GameProfile = GameProfile(
+        getInteger("gameId"),
+        getString("name"),
+        getInteger("price"),
+        getInstant("publishDate"),
+        getString("author"),
+        getString("description"),
+        getString("imageFullSize"),
+        getString("imageCardSize")
+)
+
+fun GameDetail.toJson(): JsonObject = jsonObjectOf(
+        "game" to game.toJson(),
+        "images" to images
+)
+
+fun JsonObject.toGameDetail(): GameDetail = GameDetail(
+        getJsonObject("game").toGame(),
+        getJsonArray("images").map{ it.toString() }
 )
