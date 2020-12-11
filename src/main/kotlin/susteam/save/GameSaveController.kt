55af60398fbb97d42b323d300294375d376da5e3
yinpe <11810305@mail.sustech.edu.cn>
2020-12-11 21:30:04 +0800

    fix: change gameSave: gameId -> gameKey

--- src/main/kotlin/susteam/save/GameSaveController.kt
+++ src/main/kotlin/susteam/save/GameSaveController.kt
@@ -7,18 +7,20 @@ import io.vertx.ext.web.RoutingContext
 import io.vertx.kotlin.core.json.jsonObjectOf
 import susteam.CoroutineController
 import susteam.ServiceException
+import susteam.game.GameService
 import susteam.storage.StorageService
 
 class GameSaveController @Inject constructor(
     private val service: GameSaveService,
+    private val gameService: GameService,
     private val storage: StorageService
 ) : CoroutineController() {
 
     override fun route(router: Router) {
-        router.get("/save/:username/:gameId").coroutineHandler(::handleGetAllGameSave)
-        router.post("/save/:username/:gameId/:saveName").coroutineHandler(::handleUploadGameSave)
-        router.get("/save/:username/:gameId/:saveName/delete").coroutineHandler(::handleDeleteGameSave)
-        router.get("/save/:username/:gameId/:saveName").coroutineHandler(::handleGetGameSave)
+        router.get("/save/:username/:gameKey").coroutineHandler(::handleGetAllGameSave)
+        router.post("/save/:username/:gameKey/:saveName").coroutineHandler(::handleUploadGameSave)
+        router.get("/save/:username/:gameKey/:saveName/delete").coroutineHandler(::handleDeleteGameSave)
+        router.get("/save/:username/:gameKey/:saveName").coroutineHandler(::handleGetGameSave)
         router.get("/check/:gameId").coroutineHandler(::handleCheckBought)
     }
 
@@ -26,7 +28,9 @@ class GameSaveController @Inject constructor(
         val request = context.request()
 
         val auth = context.user() ?: throw ServiceException("Permission denied, please login")
-        val gameId = request.getParam("gameId")?.toIntOrNull() ?: throw ServiceException("Game ID not found")
+        val gameKey = request.getParam("gameKey") ?: throw ServiceException("Game Key not found")
+
+        val gameId = gameService.getGameIdByGameKey(gameKey)
 
         val gameSaves = service.getAllGameSaveName(auth, gameId)
 
@@ -39,10 +43,12 @@ class GameSaveController @Inject constructor(
 
     private suspend fun handleUploadGameSave(context: RoutingContext) {
         val request = context.request()
-        val gameId = request.getParam("gameId")?.toIntOrNull() ?: throw ServiceException("Game ID is empty")
+        val gameKey = request.getParam("gameKey") ?: throw ServiceException("Game Key not found")
         val saveName = request.getParam("saveName") ?: throw ServiceException("save name is empty")
 
         val auth = context.user() ?: throw ServiceException("Permission denied, please login")
+
+        val gameId = gameService.getGameIdByGameKey(gameKey)
 
         service.checkBought(auth, gameId)
 
@@ -66,8 +72,10 @@ class GameSaveController @Inject constructor(
         val request = context.request()
 
         val auth = context.user() ?: throw ServiceException("Permission denied, please login")
-        val gameId = request.getParam("gameId")?.toIntOrNull() ?: throw ServiceException("Game ID not found")
+        val gameKey = request.getParam("gameKey") ?: throw ServiceException("Game Key not found")
         val saveName = request.getParam("saveName") ?: throw ServiceException("Save not found")
+
+        val gameId = gameService.getGameIdByGameKey(gameKey)
 
         service.deleteGameSave(auth, gameId, saveName)
 
@@ -78,8 +86,10 @@ class GameSaveController @Inject constructor(
         val request = context.request()
 
         val auth = context.user() ?: throw ServiceException("Permission denied, please login")
-        val gameId = request.getParam("gameId")?.toIntOrNull() ?: throw ServiceException("Game ID not found")
+        val gameKey = request.getParam("gameKey") ?: throw ServiceException("Game Key not found")
         val saveName = request.getParam("saveName") ?: throw ServiceException("Save not found")
+
+        val gameId = gameService.getGameIdByGameKey(gameKey)
 
         val storageFile = service.download(auth, gameId, saveName)
 
